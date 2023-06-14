42d7e273149020b4655dc27faf8699253d371889
Spyral96 <135088092+Spyral96@users.noreply.github.com>
2023-06-14 00:19:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ Created on Tue Jun  6 22:22:13 2023
 @author: Dan
 """
 #imports including pokemon desciption function
-#import pokemon_data_frame
 import random
 import requests
 from win10toast import ToastNotifier
 import smtplib
 from email.message import EmailMessage
-
-
+import os
+import pandas as pd
 ###############making txt file for debugger
 
 
@@ -22,24 +21,21 @@ from email.message import EmailMessage
 
 
 
+directory = "C:/Users/Dan/Desktop/Python Files/Debugging txt files"
+print("C:/Users/Dan/Desktop/Python Files/Debugging txt files/")
 
-# =============================================================================
-# directory = "C:/Users/Dan/Desktop/Python Files/Debugging txt files"
-# 
-# 
-# # Create the directory if it doesn't exist
-# if not os.path.exists(directory):
-#     os.makedirs(directory)
-#     
-# file_path = os.path.join(directory, "unicode.txt")
-# 
-# file_path = 'C:\Users\Dan\Desktop\Python Files\Debugging txt files'
-#  
-# with open(file_path,'a',encoding=utf-8) as Debuggerpoke:    
-#     
-# file.write("Some text\n")
-# 
-# =============================================================================
+# Create the directory if it doesn't exist
+if not os.path.exists(directory):
+    os.makedirs(directory)
+    
+file_path = os.path.join(directory, "Debugger.txt")
+
+#file_path = 'C:\\Users\Dan\Desktop\Python Files\Debugging txt files'
+ 
+with open(file_path,'a') as Debuggerpoke:    
+    
+    Debuggerpoke.write("Some text\n")
+
 
 
 #making random dex number to add to the end of api url
@@ -53,21 +49,11 @@ response = requests.get(url)
 pokemon_raw_data = response.json()
 
 
+#######
 
 
-# =============================================================================
-# print(f"Pokedex Number: {pokemon_data_frame.dex_num}")
-# print(f'\nBase Stats:\nHP: {pokemon_data_frame.base_hp}\nAttack: {pokemon_data_frame.base_att}\nDefense: {pokemon_data_frame.base_def}\nSpeaial Attack: {pokemon_data_frame.base_spa}\nSpecial Defense: {pokemon_data_frame.base_spd}\nSpeed: {pokemon_data_frame.base_speed}')
-# for x in pokemon_data_frame.type_names:
-#     print("Type: " + x.title())
-# print("Moveset:\n")
-# 
-# for x in pokemon_data_frame.movesets:
-#    print(x.title() + '\n')
-# 
-# =============================================================================
+########   Variables      #######
 
-#pokemon variables
 base_hp = pokemon_raw_data['stats'][0]["base_stat"]
 
 base_att = pokemon_raw_data['stats'][1]["base_stat"]
@@ -83,8 +69,6 @@ base_speed = pokemon_raw_data['stats'][5]["base_stat"]
 poke_name = pokemon_raw_data ["name"]
 
 type_uncleaned_data = pokemon_raw_data['types'][0:]
-
-
 
 type_names = []
 #adding stats for base stat total
@@ -109,45 +93,120 @@ for dictionary in raw_moveset:
     movesets.append(moveset)
 
 
+
+##############################This is where i started fixin things
+
+
+# start 
+
  #fetching specific move descriptions 
-#creating url list for all moves
+#creating url list for all moves (Linking to a different api)
 moves_list_url=[]
-for dictionary in raw_moveset:
+
+
+#looping throuugh
+for dictionary in raw_moveset: 
     moveset_url = dictionary['move']['url']
     moves_list_url.append(moveset_url)
 
+#print(moves_list_url)
 
-# =============================================================================
-# def email():
-#     print(f"Pokedex Number: {dex_num}\n")
-#     print(poke_name.title())
-#     print("")
-#     print(f'\nBase Stats:\nHP: {base_hp}\nAttack: {base_att}\nDefense: {base_def}\nSpeaial Attack: {base_spa}\nSpecial Defense: {base_spd}\nSpeed: {base_speed}')
-#     for x in type_names:
-#         print("Type: " + x.title())
-#     print("Moveset:\n")
-#     
-#     for x in movesets:
-#        print(x.title() + '\n')
-# 
-# text_file = email()
-# =============================================================================
+
+#looping each url to be insert in requesting
+#making json list to put in dataframe
+move_description = []
+for url in moves_list_url:
+         second_respnse = requests.get(url)
+         pokemon_moveset_specific_data = second_respnse.json()
+         move_description.append(pokemon_moveset_specific_data)
+         
+#just made the a dictionary for each moves' descriptions on "movesets"         
+         
+#fetching our values from multiple json dictionaries (move_description)
+
+#making list for each 
+
+move_power = []
+move_accuracy = []
+effect_entry = []
+move_type = []
+damage_type = []
+
+
+#looping through dictionries json values and adding them to blank lists
+for move_desc in move_description:
+    damage_type.append(move_desc['damage_class']['name'])
+    move_type.append(move_desc['type']['name'])
+    
+    effect_entries = move_desc['effect_entries']
+    if effect_entries:
+        effect_entry.append(effect_entries[0]['short_effect'])
+    else:
+        effect_entry.append("N/A")
+    
+    
+    #if power has no value!
+    if move_desc['power'] is None:
+        move_desc['power'] = "N/A"
+        move_power.append(move_desc['power'])
+    else:
+        move_power.append(move_desc['power'])
+        
+    #if accuracy has no value
+    if move_desc['accuracy'] is None:
+        move_desc['accuracy'] = "N/A"
+        move_accuracy.append(move_desc['accuracy'])
+    
+    else:
+        move_accuracy.append(move_desc['accuracy'])
+
+#making a dictionary with the values(lists(line 95-99)) i want to turn it into data frame
+
+move_detail_dict = {'moveset': movesets,
+                    'move_types': move_type,        
+                    'move_powers':move_power,
+                    'move_accuracys':move_accuracy,
+                    'effect_entrys': effect_entry,                            
+                    'damage_types':damage_type
+}
+
+move_df = pd.DataFrame(move_detail_dict,columns= ['moveset','move_types','move_powers','move_accuracys','effect_entrys','damage_types'])
+
+
+
 
 #creating ,txt file
 with open('Pokemon Daily Update.txt', 'w') as file:
     # Write the content to the file
-    file.write(f"Pokedex Number: {dex_num}\n")
-    file.write("\n")
+    
     file.write(poke_name.title())
     file.write("\n")
     for x in type_names:
         file.write("Type: " + x.title())
     file.write("\n")
+    file.write(f"Pokedex Number: {dex_num}")
+    file.write(f'\nBase Stats:\nHP: {base_hp}\nAttack: {base_att}\nDefense: {base_def}\nSpeaial Attack: {base_spa}\nSpecial Defense: {base_spd}\nSpeed: {base_speed}')
+    
+    file.write('\n')
     file.write("Moveset:\n")
-    file.write("\n")
-    for x in movesets:
-        file.write(x.title() + '\n')
+    file.write('\n')
+    for move,typing,power,acc,entry,dam_type  in zip(move_df['moveset'],move_df['move_types'],move_df['move_powers'],move_df['move_accuracys'],move_df['effect_entrys'],move_df['damage_types']):
+        file.write(f'{move.title()}\n Type: {typing.title()}\n Power: {power}\n Accuracy: {acc}\n Damage Type: {dam_type.title()}\n Description: {entry} \n \n')
 
+# =============================================================================
+#     file.write(f"Pokedex Number: {dex_num}\n")
+#     file.write("\n")
+#     file.write(poke_name.title())
+#     file.write("\n")
+#     for x in type_names:
+#         file.write("Type: " + x.title())
+#     file.write("\n")
+#     file.write("Moveset:\n")
+#     file.write("\n")
+#     for x in movesets:
+#         file.write(x.title() + '\n')
+# 
+# =============================================================================
 
 
  
@@ -177,7 +236,7 @@ with smtplib.SMTP('smtp.gmail.com',587) as smtp:
      smtp.starttls()
      smtp.ehlo()
      
-     #smtp.login('your email','password')
+     smtp.login('your email','your password')
      
     
      #send email  
@@ -189,22 +248,3 @@ print("email has sent")
 
 with open('Sucess.txt', 'w') as confirm:
     confirm.write("the email has been sent")
-
-
-
-# =============================================================================
-# server = smtplib.SMTP('smtp.gmail,com',465)
-# 
-# server.starttls()
-# 
-# server.login()
-# 
-# server.sendmail("your email",'sending to')
-# 
-# print("email has sent")
-# 
-# =============================================================================
-
-
-
-
